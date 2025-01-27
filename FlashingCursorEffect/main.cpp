07479fe427ac18513ecdec94a77de54c7f0361b2
jjosephcoder <jjosephuk@outlook.com>
2025-01-27 15:55:24 +0000

    Add Flashing Cursor Effect Function

--- FlashingCursorEffect/main.cpp
+++ FlashingCursorEffect/main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
-
-
-
-
-
-
+#include <windows.h> //Library needed for Sleep method
+#include <vector>
 
 using namespace std;
+///Function Prototypes
+void flashcursor(string &output);
+
+void flashcursor(string &output){
+    unsigned char block = 254;
+    
+    for(size_t i=0; i<output.size(); i++){
+            cout << output[i];
+            cout << block;
+            Sleep(250);
+            cout << "\b";
+            cout << " ";
+            Sleep(250);
+            cout << "\b";
+           
+        }
+    }
+
+
 
 int main() {
-	cout << "hello world!" << endl;
+    string output={};
+  	cout << "Please enter a string" << endl;
+    getline(cin,output);
+    flashcursor(output);
 	return 0;
 }
